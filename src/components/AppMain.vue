7b5b5a5916b1796c26776764e7a40f44f2a51fd8
lorenzodisanto <lore.disanto95@gmail.com>
2024-02-13 15:53:48 +0100

    feat: add main content and props

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,147 @@
 <script>
-export default {};
+import AppCard from "./AppCard.vue";
+
+export default {
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          frontImage: "1.webp",
+          backImage: "1b.webp",
+          brand: "Levi's",
+          name: "Relaxed Fit",
+          price: 29.99,
+          isInFavorites: true,
+          badges: [
+            {
+              type: "tag",
+              value: "Sostenibilità",
+            },
+            {
+              type: "discount",
+              value: "-50%",
+            },
+          ],
+        },
+        {
+          id: 2,
+          frontImage: "2.webp",
+          backImage: "2b.webp",
+          brand: "Guess",
+          name: "Roses Tee",
+          price: 20.99,
+          isInFavorites: true,
+          badges: [
+            {
+              type: "discount",
+              value: "-30%",
+            },
+          ],
+        },
+        {
+          id: 3,
+          frontImage: "3.webp",
+          backImage: "3b.webp",
+          brand: "Come Zucchero Filato",
+          name: "Voglia di colori pastello",
+          price: 129.99,
+          isInFavorites: false,
+          badges: [
+            {
+              type: "discount",
+              value: "-30%",
+            },
+          ],
+        },
+        {
+          id: 4,
+          frontImage: "4.webp",
+          backImage: "4b.webp",
+          brand: "Levi's",
+          name: "Tee Unisex",
+          price: 14.99,
+          isInFavorites: false,
+          badges: [
+            {
+              type: "tag",
+              value: "Sostenibilità",
+            },
+            {
+              type: "discount",
+              value: "-50%",
+            },
+          ],
+        },
+        {
+          id: 5,
+          frontImage: "5.webp",
+          backImage: "5b.webp",
+          brand: "Maya Deluxe",
+          name: "Stripe Bodice",
+          price: 99.99,
+          isInFavorites: true,
+          badges: [
+            {
+              type: "tag",
+              value: "Sostenibilità",
+            },
+            {
+              type: "discount",
+              value: "-50%",
+            },
+          ],
+        },
+        {
+          id: 6,
+          frontImage: "6.webp",
+          backImage: "6b.webp",
+          brand: "Esprit",
+          name: "Maglione - Black",
+          price: 29.99,
+          isInFavorites: true,
+          badges: [
+            {
+              type: "tag",
+              value: "Sostenibilità",
+            },
+          ],
+        },
+      ],
+    };
+  },
+
+  methods: {
+    buildImagePath(imageName) {
+      const imageUrl = new URL("../assets/img/" + imageName, import.meta.url);
+      return imageUrl.href;
+    },
+  },
+
+  components: { AppCard },
+};
 </script>
 
 <template>
-  <main>App Main</main>
+  <main>
+    <div class="container products">
+      <app-card
+        v-for="product in products"
+        :imgFront="buildImagePath(product.frontImage)"
+        :imgBack="buildImagePath(product.backImage)"
+        :titleBrand="product.brand"
+        :titleName="product.name"
+        :oldPrice="product.price"
+        :prodBadges="product.badges"
+      ></app-card>
+    </div>
+  </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.products {
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 50px;
+}
+</style>
