2f2588e0d851f833e825e3a49953a7d845d11108
lorenzodisanto <lore.disanto95@gmail.com>
2024-02-13 15:53:33 +0100

    feat: add header content and props

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,60 @@
 <script>
-export default {};
+export default {
+  props: {
+    links: Array,
+  },
+};
 </script>
 
 <template>
-  <header>App Header</header>
+  <header>
+    <div class="container">
+      <div>
+        <ul>
+          <li v-for="link in links">
+            <a :href="link.url">{{ link.text }}</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <img src="../assets/img/boolean-logo.png" alt="" />
+      </div>
+      <div>
+        <ul>
+          <li>
+            <a href="#"><i class="fa-regular fa-user"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa-regular fa-heart"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa-solid fa-bag-shopping"></i></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  min-height: 75px;
+  background-color: #ff6900;
+  box-shadow: 0px 2px 3px 0px #555;
+  position: sticky;
+  top: 0;
+  z-index: 3;
+
+  .container {
+    min-height: 75px;
+  }
+
+  a {
+    color: white;
+  }
+
+  img {
+    width: 150px;
+  }
+}
+</style>
